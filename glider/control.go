b8fe563070319dc0136f2cadf2b9cb5e87090692
Brandon Skari <brandon@skari.org>
2020-09-26 01:14:51 -0600

    Add servo control for ailerons

---
+++ glider/control.go
@@ -0,0 +1,54 @@
+// Control for the ailerons
+package glider
+
+import (
+	"github.com/stianeikeland/go-rpio/v4"
+	"errors"
+)
+
+const LEFT_SERVO_PIN = 18  // BCM 18 = board 12
+const RIGHT_SERVO_PIN = 13  // BCM 13 = board 33
+const HERTZ = 50
+const MULTIPLIER = 100000
+const CENTER_MS = 1400
+const NINETY_DEGREES_MS = 400
+
+type Control struct {
+	left rpio.Pin
+	right rpio.Pin
+}
+
+func NewControl() *Control {
+	control := Control{
+		left: rpio.Pin(LEFT_SERVO_PIN),
+		right: rpio.Pin(RIGHT_SERVO_PIN),
+	}
+	control.left.Mode(rpio.Pwm)
+	control.right.Mode(rpio.Pwm)
+	// Param freq should be in range 4688Hz - 19.2MHz to prevent
+	// unexpected behavior
+	control.left.Freq(HERTZ * MULTIPLIER)
+	control.right.Freq(HERTZ * MULTIPLIER)
+	return &control
+}
+
+func (control *Control) Set(angle Degrees) error {
+	// Output frequency is computed as pwm clock frequency divided by cycle length.
+	// So, to set Pwm pin to freqency 38kHz with duty cycle 1/4, use this combination:
+	//  pin.DutyCycle(1, 4)
+	//  pin.Freq(38000*4)
+	if angle < 30 || angle > 150 {
+		return errors.New("Bad angle")
+	}
+	targetUs := uint32((float32(angle) - 90.0) * NINETY_DEGREES_MS + CENTER_MS)
+	a := getDutyCycleForUs(targetUs, HERTZ, MULTIPLIER)
+	control.left.DutyCycle(a, MULTIPLIER)
+	return nil
+}
+
+func getDutyCycleForUs(targetUs, frequencyHz, multiplier uint32) uint32 {
+	// We want a / multiplier = duty / cycle, where cycle = 1s / Hz
+	// a / multiplier = duty / (1,000,000 us / 50 hz) = 20,000 us
+	// a = duty / (1,000,000 us / 50 hz) * multiplier
+	return uint32(float32(targetUs) / (float32(1000 * 1000) / float32(frequencyHz)) * float32(multiplier))
+}
