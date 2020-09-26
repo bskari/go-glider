054c4257d943c42a8737c41529eb2e47a17f6dca
Brandon Skari <brandon@skari.org>
2020-09-26 11:39:10 -0600

    Fix tests

--- glider/telemetry_test.go
+++ glider/telemetry_test.go
@@ -11,23 +11,23 @@ func TestParseSentence(t *testing.T) {
 		t.Errorf("Failed to create telemetry: %v", err)
 	}
 	telemetry.parseSentence("$GPRMC,081836,A,3700.00,N,13300.00,W,000.0,360.0,130998,011.3,E*69")
-	if telemetry.previousPoint.Latitude != 37.0 {
+	if telemetry.recentPoint.Latitude != 37.0 {
 		t.Error("Failed to parse RMC latitude")
 	}
-	if telemetry.previousPoint.Longitude != -133.0 {
+	if telemetry.recentPoint.Longitude != -133.0 {
 		t.Error("Failed to parse RMC longitude")
 	}
 
 	telemetry.parseSentence("$GPGGA,134658.00,4300.00,S,04000,E,2,09,1.0,1048.47,M,-16.27,M,08,AAAA*43")
-	if telemetry.previousPoint.Latitude != -43.0 {
+	if telemetry.recentPoint.Latitude != -43.0 {
 		t.Error("Failed to parse GGA latitude")
 	}
-	if telemetry.previousPoint.Longitude != 40.0 {
+	if telemetry.recentPoint.Longitude != 40.0 {
 		t.Error("Failed to parse GGA longitude")
 	}
 
 	telemetry.parseSentence("$GPVTG,054.7,T,034.4,M,005.5,N,007.2,K*4E")
-	if telemetry.lastMps != 2 {
+	if telemetry.recentMps != 2 {
 		t.Error("Failed to parse VTG mps")
 	}
 }
@@ -81,9 +81,9 @@ func TestGetPosition(t *testing.T) {
 		$GPVTG,054.7,T,034.4,M,005.5,N,007.2,K*4E\n`)
 
 	telemetry := Telemetry{
-		previousPoint: Point{Latitude: 40.0, Longitude: -105.2, Altitude: 1655},
-		lastMps:       0.0,
-		gps:           bufio.NewReader(fakeGps),
+		recentPoint: Point{Latitude: 40.0, Longitude: -105.2, Altitude: 1655},
+		recentMps:   0.0,
+		gps:         bufio.NewReader(fakeGps),
 	}
 
 	telemetry.GetPosition()
