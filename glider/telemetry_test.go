efd5dba6e79f3c0c974d1c164ef824537dccc834
Brandon Skari <brandon@skari.org>
2020-09-26 01:15:24 -0600

    Add telemetry tests

--- glider/telemetry_test.go
+++ glider/telemetry_test.go
@@ -6,20 +6,23 @@ import (
 )
 
 func TestParseSentence(t *testing.T) {
-	telemetry := NewTelemetry()
+	telemetry, err := NewTelemetry()
+	if err != nil {
+		t.Errorf("Failed to create telemetry: %v", err)
+	}
 	telemetry.parseSentence("$GPRMC,081836,A,3700.00,N,13300.00,W,000.0,360.0,130998,011.3,E*69")
-	if telemetry.previousPoint.Latitude_d != 37.0 {
+	if telemetry.previousPoint.Latitude != 37.0 {
 		t.Error("Failed to parse RMC latitude")
 	}
-	if telemetry.previousPoint.Longitude_d != -133.0 {
+	if telemetry.previousPoint.Longitude != -133.0 {
 		t.Error("Failed to parse RMC longitude")
 	}
 
 	telemetry.parseSentence("$GPGGA,134658.00,4300.00,S,04000,E,2,09,1.0,1048.47,M,-16.27,M,08,AAAA*43")
-	if telemetry.previousPoint.Latitude_d != -43.0 {
+	if telemetry.previousPoint.Latitude != -43.0 {
 		t.Error("Failed to parse GGA latitude")
 	}
-	if telemetry.previousPoint.Longitude_d != 40.0 {
+	if telemetry.previousPoint.Longitude != 40.0 {
 		t.Error("Failed to parse GGA longitude")
 	}
 
@@ -30,7 +33,10 @@ func TestParseSentence(t *testing.T) {
 }
 
 func BenchmarkParseSentence(b *testing.B) {
-	telemetry := NewTelemetry()
+	telemetry, err := NewTelemetry()
+	if err != nil {
+		b.Errorf("Failed to create telemetry: %v", err)
+	}
 	for i := 0; i < b.N; i++ {
 		telemetry.parseSentence("$GPRMC,081836,A,3700.00,N,13300.00,W,000.0,360.0,130998,011.3,E*69")
 		telemetry.parseSentence("$GPGGA,134658.00,4300.00,S,04000,E,2,09,1.0,1048.47,M,-16.27,M,08,AAAA*43")
@@ -75,7 +81,7 @@ func TestGetPosition(t *testing.T) {
 		$GPVTG,054.7,T,034.4,M,005.5,N,007.2,K*4E\n`)
 
 	telemetry := Telemetry{
-		previousPoint: Point{Latitude_d: 40.0, Longitude_d: -105.2, Altitude_m: 1655},
+		previousPoint: Point{Latitude: 40.0, Longitude: -105.2, Altitude: 1655},
 		lastMps:       0.0,
 		gps:           bufio.NewReader(fakeGps),
 	}
